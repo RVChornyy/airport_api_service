7e1d7d3d8291bece3d5ab03e21c24d5019bf4f40
Rost <rv4@ukr.net>
2023-12-12 00:58:12 +0200

    add function image_file_path

--- utilities/image_file_paths.py
+++ utilities/image_file_paths.py
@@ -6,6 +6,6 @@ from django.utils.text import slugify
 
 def airplane_image_file_path(instance, filename):
     _, extension = os.path.splitext(filename)
-    filename = f"{slugify(instance.type)}-{uuid.uuid4()}{extension}"
+    filename = f"{slugify(instance.call_sign)}-{uuid.uuid4()}{extension}"
 
     return os.path.join("uploads/airplanes/", filename)
